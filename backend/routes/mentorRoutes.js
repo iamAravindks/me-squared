c44982aff6513d6356d05f554e9386d320ab7649
iamAravindks <aravindkadaikattu@gmail.com>
2022-06-19 15:25:58 +0530

    add post mentors endpoint without auth

--- backend/routes/mentorRoutes.js
+++ backend/routes/mentorRoutes.js
@@ -27,3 +27,35 @@ mentorRouter.get(
   })
 );
 
+
+// @desc create a new mentor
+// @route /api/mentors
+// @access private
+
+mentorRouter.post('/', expressAsyncHandler(async (req, res) =>
+{
+  try {
+    const newMentor = {
+      name: req.body.name,
+      designation: req.body.designation,
+      yearNdClass: req.body.yearNdClass,
+      respondIn: req.body.respondIn,
+      about: req.body.about,
+      tags: req.body.tags,
+      socialLinks: req.body.socialLinks,
+      watNum: req.body.watNum,
+      eMail:req.body.eMail
+    }
+    const createdMentor = Mentors.create(newMentor)
+    if (createdMentor)
+    {
+      res.status(201).json({
+        message: "mentor created",
+        data:newMentor
+      })
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}))
+
