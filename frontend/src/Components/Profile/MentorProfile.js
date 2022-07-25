a47791bff2155e7547193cba367e54f77b389d11
AthulJoshi <athuljoshi7@gmail.com>
2022-07-25 09:09:52 +0530

    mentorprofile changes

--- frontend/src/Components/Profile/MentorProfile.js
+++ frontend/src/Components/Profile/MentorProfile.js
@@ -1,38 +1,45 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react'
-import { useContext, useEffect } from 'react'
+import React from 'react';
+import { useContext, useEffect } from 'react';
 
-import { MentorContext } from "../../context/mentorContext/Context";
+import { MentorContext } from '../../context/mentorContext/Context';
 
-import styles from './mentorProfile.module.css'
+import styles from './mentorProfile.module.css';
 
 const MentorProfile = () => {
+  const { getFollowRequests, getProfileMentor, mentorsState } =
+    useContext(MentorContext);
+  const { userMentor, followReqs } = mentorsState;
 
-    const { getFollowRequests } = useContext(MentorContext);
-    useEffect(()=>{
-        getFollowRequests ()
-    },[])
+  useEffect(() => {
+    getProfileMentor();
+    getFollowRequests();
+  }, []);
 
-    return (
-        <div>
-            <div className={styles.banner}>
-                <p></p>
+  return (
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <p></p>
+      </div>
+
+      <div className={styles.bContainer}>
+        <div className={styles.left}>
+          <div className={styles.profContainer}>
+            <div className={styles.profCard}>
+              <img src={userMentor.profileImage} alt="blaah" />
+              <h2>{userMentor.name}</h2>
+              <p>{userMentor.designation}</p>
             </div>
-            <div className={styles.left}>
-                <div className={styles.profContainer}>
-                    <div className={styles.profCard}>
-                        <h2>Benjamin Hoppe</h2>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.right}>
-                <div className={styles.requestCard}>
-
-                </div>
-            </div>
-
+          </div>
         </div>
-    )
-}
+        <div className={styles.right}>
+          <div className={styles.reqCard}>
+            <p>{followReqs}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MentorProfile
+export default MentorProfile;
