8bd4fa3730a1a5fbffd7ec0e630b360a683f4652
iamAravindks <aravindkadaikattu@gmail.com>
2022-08-01 20:00:32 +0530

    change path to * in express app

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import path from "path";
+
 import connectDB from "./config/db.js";
 import { mentorRouter } from "./routes/mentorRoutes.js";
 import menteeRoute from "./routes/menteeRoute.js";
 import { errorHandler, notFound } from "./middleware/errorHanlder.js";
 
+const __dirname = path.resolve();
 
 
 const app = express()
@@ -26,6 +29,12 @@ app.use(morgan("dev"))
 app.use("/api/mentors", mentorRouter);
 app.use("/api/mentees",menteeRoute)
 // app.get("/", (req, res) => res.send("Hello World!"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.use(notFound)
 app.use(errorHandler)
